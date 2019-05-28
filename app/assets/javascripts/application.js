6fba23488e990105ab590a9e7511ed821009e508
Jose Arellano <josearellano@me.com>
2019-05-28 03:51:30 +0000

    b03_7_Add-Bootstrap_

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//=r equire bootstrap-sprockets
 //= require_tree .
